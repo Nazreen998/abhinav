6e120706df98e47fcb89efb0f4037ffe4a07a0c7
Nazreen998 <nazreenbegam1998@gmail.com>
2025-10-10 10:01:16 +0530

    java script added

---
+++ script.js
@@ -0,0 +1,13 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.querySelectorAll(".slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = "0";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.opacity = "1";
+  setTimeout(showSlides, 4000); // 4 seconds per slide
+}
